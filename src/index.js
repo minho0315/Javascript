10168cb789c34eea7d76ad079109ff758a293949
minho <62683035+minho0315@users.noreply.github.com>
2021-03-11 02:01:32 +0900

    variable

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
-console.log("안녕하세요");
-console.log("JavaScript 를 배워봅시다!");
+// console.log("안녕하세요");
+// console.log("JavaScript 를 배워봅시다!");
+
+// // 변수
+// let value = 1;
+// console.log(value);
+// value = 2;
+// console.log(value);
+
+// // 상수
+// const a = 1;
+// a = 2; // 오류
+
+// var는 사용x
+
+// 데이터 타입
+// 숫자(Number)
+let value = 1;
+
+// 문자열
+let text = 'hello';
+let name = '자바스크립트';
+
+// 참/거짓 (Boolean)
+let good = true;
+let loading = false;
+
+// null과 undefined
+// null은 값이 없다 ( 난 친구 같은 거 없어)
+const friend = null;
+console.log(friend);
+
+// undefined는 아직 값이 설정되지 않은 것
+let criminal;
+console.log(criminal);
+
+// null 과 undefined는 둘 다 값이 없음을 의미하는건 맞는데, null 은 우리가 없다고,
+// 고의적으로 설정하는 값을 의미하고, undefined는 우리가 설정하지 않았기 때문에
+// 없는 값을 의미한다.

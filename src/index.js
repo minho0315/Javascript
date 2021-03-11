cadf226c270e84a3c2232e83c93da6ad0b3058f3
minho <62683035+minho0315@users.noreply.github.com>
2021-03-11 21:06:39 +0900

    함수

--- src/index.js
+++ src/index.js
@@ -165,7 +165,9 @@
 // const b = '하세요';
 // console.log(a + b);
 
-// // 04. 조건문
+/**
+ * 04. 조건문
+ */
 // // if문
 // const a = 1;
 // if (a + 1 === 2) {
@@ -213,3 +215,71 @@
 //   default:
 //     console.log('모르겠네요..');
 // }
+
+/**
+ * 05. 함수
+ */
+// const a = 1;
+// const b = 2;
+// const sum = a + b;
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const sum = add(1, 2);
+// console.log(sum);
+
+// function add(a, b) {
+//   return a + b;
+//   console.log('호출이 되지 않는 코드!');
+// }
+
+// const sum = add(1, 2);
+// console.log(sum);
+
+// // 연습
+// function hello(name) {
+//   console.log('Hello, ' + name + '!');
+// }
+// hello('velopert');
+
+// // 템플릿 리터럴
+// function hello(name) {
+//   console.log(`Hello, ${name}!`);
+// }
+// hello('velopert');
+
+// // 점수를 성적등급으로 변환하기
+// function getGrade(score) {
+//   if (score === 100) {
+//     return 'A+';
+//   } else if (score >= 90) {
+//     return 'A';
+//   } else if (score === 89) {
+//     return 'B+';
+//   } else if (score >= 80) {
+//     return 'B';
+//   } else if (score === 79) {
+//     return 'C+';
+//   } else if (score >= 70) {
+//     return 'C';
+//   } else if (score === 69) {
+//     return 'D+';
+//   } else if (score >= 60) {
+//     return 'F';
+//   }
+// }
+
+// const grade = getGrade(90);
+// console.log(grade);
+
+// // 화살표 함수
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(add(1, 2));
+
+// const add = (a, b) => a + b;
+// console.log(add(1, 2));

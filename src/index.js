77f61e01277dfd8c86835b2d88bd0cd52b848a34
minho <62683035+minho0315@users.noreply.github.com>
2021-03-11 21:48:46 +0900

    배열

--- src/index.js
+++ src/index.js
@@ -462,3 +462,34 @@
 // console.log(numbers.sum);
 // console.log(numbers.sum);
 // console.log(numbers.sum);
+
+/**
+ *  07. 배열
+ */
+// const array = [1, 2, 3, 4, 5];
+
+// const objects = [{ name: '멍멍이' }, { name: '야옹이' }];
+
+// console.log(objects);
+// console.log(objects[0]);
+// console.log(objects[1]);
+
+// // 배열에 새 항목 추가하기
+// const objects = [{ name: '멍멍이' }, { name: '야옹이' }];
+
+// objects.push({
+//   name: '멍뭉이'
+// });
+
+// console.log(objects);
+
+// // 배열의 크기 알아내기
+// const objects = [{ name: '멍멍이' }, { name: '야옹이' }];
+
+// console.log(objects.length);
+
+// objects.push({
+//   name: '멍뭉이'
+// });
+
+// console.log(objects.length);

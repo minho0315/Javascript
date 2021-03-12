b03f09dfc5995a1226d1cbdc60113e78b886b18d
minho <62683035+minho0315@users.noreply.github.com>
2021-03-12 18:12:15 +0900

    조건문 더 스마트하게 쓰기

--- src/index.js
+++ src/index.js
@@ -246,3 +246,83 @@
 
 // const area = calculateCircleArea();
 // console.log(area);
+
+/**
+ * 05. 조건문 더 스마트하게 쓰기
+ */
+// 특정 값이 여러 값중 하나인지 확인해야 할 때
+// function isAnimal(text) {
+//   return (
+//     text === '고양이' || text === '개' || text === '거북이' || text === '너구리'
+//   );
+// }
+// console.log(isAnimal('개'));
+// console.log(isAnimal('노트북'));
+
+// function isAnimal(name) {
+//   const animals = ['고양이', '개', '거북이', ' 너구리'];
+//   return animals.includes(name);
+// }
+// console.log(isAnimal('개'));
+// console.log(isAnimal('노트북'));
+
+// 값에 따라 다른 결과물을 반환 해야 할 때
+// function getSound(animal) {
+//   if (animal === '개') return '멍멍!';
+//   if (animal === '고양이') return '야옹~';
+//   if (animal === '참새') return '짹짹';
+//   if (animal === '비둘기') return '구구 구 구';
+//   return '...?';
+// }
+// console.log(getSound('개'));
+// console.log(getSound('비둘기'));
+
+// function getSound(animal) {
+//   switch (animal) {
+//     case '개':
+//       return '멍멍!';
+//     case '고양이':
+//       return '야옹~';
+//     case '참새':
+//       return '짹짹';
+//     case '비둘기':
+//       return '구구 구 구';
+//     default:
+//       return '...?';
+//   }
+// }
+// console.log(getSound('개'));
+// console.log(getSound('비둘기'));
+
+// function getSound(animal) {
+//   const sounds = {
+//     개: '멍멍!',
+//     고양이: '야옹~',
+//     참새: '짹짹',
+//     비둘기: '구구 구 구'
+//   };
+//   return sounds[animal] || '...?';
+// }
+// console.log(getSound('개'));
+// console.log(getSound('비둘기'));
+
+// function makeSound(animal) {
+//   const tasks = {
+//     개() {
+//       console.log('멍멍');
+//     },
+//     고양이() {
+//       console.log('야옹');
+//     },
+//     비둘기() {
+//       console.log('구구 구 구');
+//     }
+//   };
+//   if (!tasks[animal]) {
+//     console.log('...?');
+//   }
+//   tasks[animal]();
+// }
+
+// makeSound('개');
+// makeSound('비둘기');

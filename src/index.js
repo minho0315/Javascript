b01c4163d1b723fff23917781ddff6c4077a92d9
minho <62683035+minho0315@users.noreply.github.com>
2021-03-12 14:31:54 +0900

    삼항 연산자

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/**
+ * 알고 있으면 유용한 자바스크립트 문법
+ */
+/**
+ * 01. 삼항 연산자
+ */
+// const array = [];
+// let text = '';
+// if (array.length === 0) {
+//   text = '배열이 비어있습니다.';
+// } else {
+//   text = '배열이 비어있지 않습니다.';
+// }
+// console.log(text);
+
+// const array = [];
+// let text =
+//   array.length === 0 ? '배열이 비어있습니다' : '배열이 비어있지 않습니다';
+// console.log(text);
+
+// 조건 ? true일때 : false일때
+
+// const array = [];
+// let text =
+//   array.length === 0 ? '배열이 비어있습니다' : '배열이 비어있지 않습니다';
+// console.log(text);
+
+// const condition1 = false;
+// const condition2 = false;
+
+// const value = condition1 ? '와우!' : condition2 ? 'blabla' : 'food';
+// console.log(value);

9556fb45884c750cfd2f6d2f46cf79b5e74fc3f7
minho <62683035+minho0315@users.noreply.github.com>
2021-03-12 21:33:39 +0900

    async/await

--- src/index.js
+++ src/index.js
@@ -87,21 +87,21 @@
 //     console.log(error);
 //   });
 
-function increaseAndPrint(n) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const value = n + 1;
-      if (value === 5) {
-        const error = new Error();
-        error.name = 'ValueIsFiveError';
-        reject(error);
-        return;
-      }
-      console.log(value);
-      resolve(value);
-    }, 1000);
-  });
-}
+// function increaseAndPrint(n) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const value = n + 1;
+//       if (value === 5) {
+//         const error = new Error();
+//         error.name = 'ValueIsFiveError';
+//         reject(error);
+//         return;
+//       }
+//       console.log(value);
+//       resolve(value);
+//     }, 1000);
+//   });
+// }
 // increaseAndPrint(0).then((n) => {
 //   console.log('result: ', n);
 // });
@@ -138,3 +138,112 @@ function increaseAndPrint(n) {
 //   .catch((e) => {
 //     console.error(e);
 //   });
+
+/**
+ * 02. async/await
+ */
+// Promise를 더욱 쉽게 사용
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+// async function process() {
+//   console.log('안녕하세요!');
+//   await sleep(1000); //1초 쉬고
+//   console.log('반갑습니다!');
+// }
+// // process();
+// process().then(() => {
+//   console.log('작업이 끝났어요!');
+// });
+
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// async function makeError() {
+//   await sleep(1000);
+//   const error = new Error();
+//   throw error;
+// }
+
+// async function process() {
+//   try {
+//     await makeError();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// process();
+
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// const getDog = async () => {
+//   await sleep(1000);
+//   return '멍멍이';
+// };
+
+// const getRabbit = async () => {
+//   await sleep(500);
+//   return '토끼';
+// };
+
+// const getTurtle = async () => {
+//   await sleep(3000);
+//   return '거북이';
+// };
+
+// async function process() {
+//   const dog = await getDog();
+//   console.log(dog);
+//   const rabbit = await getRabbit();
+//   console.log(rabbit);
+//   const turtle = await getTurtle();
+//   console.log(turtle);
+// }
+
+// process();
+
+// function sleep(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// const getDog = async () => {
+//   await sleep(1000);
+//   return '멍멍이';
+// };
+
+// const getRabbit = async () => {
+//   await sleep(500);
+//   return '토끼';
+// };
+
+// const getTurtle = async () => {
+//   await sleep(3000);
+//   return '거북이';
+// };
+
+// async function process() {
+//   const results = await Promise.all([getDog(), getRabbit(), getTurtle()]);
+//   console.log(results);
+// }
+
+// async function process() {
+//   const [dog, rabbit, turtle] = await Promise.all([
+//     getDog(),
+//     getRabbit(),
+//     getTurtle()
+//   ]);
+//   console.log(dog);
+//   console.log(rabbit);
+//   console.log(turtle);
+// }
+
+// async function process() {
+//   const first = await Promise.race([getDog(), getRabbit(), getTurtle()]);
+//   console.log(first);
+// }
+
+// process();

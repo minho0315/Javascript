9affc1423566a66ed46335efca97e2db1318eebb
minho <62683035+minho0315@users.noreply.github.com>
2021-03-12 18:41:51 +0900

    비구조화 할당 (구조 분해) 문법

--- src/index.js
+++ src/index.js
@@ -326,3 +326,118 @@
 
 // makeSound('개');
 // makeSound('비둘기');
+
+/**
+ * 비구조화 할당 (구조분해) 문법
+ */
+// const object = { a: 1, b: 2 };
+
+// const { a, b } = object;
+
+// console.log(a);
+// console.log(b);
+
+// const object = { a: 1, b: 2 };
+
+// function print({ a, b }) {
+//   console.log(a);
+//   console.log(b);
+// }
+// print(object);
+
+// const object = { a: 1 };
+
+// function print({ a, b }) {
+//   console.log(a);
+//   console.log(b);
+// }
+// print(object);
+
+// 비구조화 할당시 기본값 설정
+// const object = { a: 1 };
+
+// function print({ a, b = 2 }) {
+//   console.log(a);
+//   console.log(b);
+// }
+// print(object);
+
+// const object = { a: 1 };
+// const { a, b = 2 } = object;
+// console.log(a);
+// console.log(b);
+
+// 비구조화 할당시 이름 바꾸기
+// const animal = {
+//   name: '멍멍이',
+//   type: '개'
+// };
+// const nickname = animal.name;
+// console.log(nickname);
+
+// const animal = {
+//   name: '멍멍이',
+//   type: '개'
+// };
+// const { name: nickname } = animal; // animal 객체 안에 있는 name을 nickname이라고 선언하겠다.
+// console.log(nickname);
+
+// 배열 비구조화 할당
+// const array = [1, 2];
+// const [one, two] = array;
+
+// console.log(one);
+// console.log(two);
+
+// const array = [1];
+// const [one, two = 2] = array;
+
+// console.log(one);
+// console.log(two);
+
+// 깊은 값 비구조화 할당
+// const deepObject = {
+//   state: {
+//     information: {
+//       name: 'velopert',
+//       languages: ['korean', 'english', 'chinese']
+//     }
+//   },
+//   value: 5
+// };
+
+// const { name, languages } = deepObject.state.information;
+// const { value } = deepObject;
+
+// const extracted = {
+//   name, //object-shorthand
+//   languages,
+//   value
+// };
+
+// console.log(extracted);
+
+// const deepObject = {
+//   state: {
+//     information: {
+//       name: 'velopert',
+//       languages: ['korean', 'english', 'chinese']
+//     }
+//   },
+//   value: 5
+// };
+
+// const {
+//   state: {
+//     information: { name, languages }
+//   },
+//   value
+// } = deepObject;
+
+// const extracted = {
+//   name, //object-shorthand
+//   languages,
+//   value
+// };
+
+// console.log(extracted);

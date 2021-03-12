86dcab15efbdac3c6f252a309092d6597e05cac1
minho <62683035+minho0315@users.noreply.github.com>
2021-03-12 21:43:02 +0900

    카운터

---
+++ src/index.js
@@ -0,0 +1,47 @@
+/**
+ * HTML과 JavaSCript 연동하기
+ */
+/**
+ * 01. 카운터
+ */
+// const number = document.getElementById('number');
+// const increase = document.getElementById('increase');
+// const decrease = document.getElementById('decrease');
+
+// console.log(number);
+// console.log(increase);
+// console.log(decrease);
+
+// const number = document.getElementById('number');
+// const increase = document.getElementById('increase');
+// const decrease = document.getElementById('decrease');
+
+// console.log(number.innerText);
+// console.log(increase.offsetTop);
+// console.log(decrease.id);
+
+// const number = document.getElementById('number');
+// const increase = document.getElementById('increase');
+// const decrease = document.getElementById('decrease');
+
+// increase.onclick = () => {
+//   console.log('increase 가 클릭됨');
+// };
+
+// decrease.onclick = () => {
+//   console.log('decrease 가 클릭됨');
+// };
+
+// const number = document.getElementById('number');
+// const increase = document.getElementById('increase');
+// const decrease = document.getElementById('decrease');
+
+// increase.onclick = () => {
+//   const current = parseInt(number.innerText, 10);
+//   number.innerText = current + 1;
+// };
+
+// decrease.onclick = () => {
+//   const current = parseInt(number.innerText, 10);
+//   number.innerText = current - 1;
+// };
